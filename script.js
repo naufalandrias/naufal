4765d37de3ce4b4d03eace26a8f76aff14c38a21
naufalandrias <naufalandrias915@gmail.com>
2025-10-28 11:28:05 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+cont tombolSapa = document.getElemenByld('sapaButton');
+// 2. Tambahkan 'event listiner' untuk 'click'
+tombolsapa.addEvenlistener('click', function() {
+  // 3. jalankan kode ini ketika tombol di-klik
+                           alert('Halo! Terima kasih sudah berkunjung!');
+  {(;
